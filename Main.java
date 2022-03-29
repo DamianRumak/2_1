a87825bd1f57532a0155a5e0ec802573b99b8cca
DamianRumak1 <100698199+DamianRumak@users.noreply.github.com>
2022-03-29 12:35:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int x=0;
+    while(x<10){
+      x++;
+        for(int y=0; y<x; y++)
+          System.out.print("*");
+      System.out.println();
+    }
+    
+}
+}
